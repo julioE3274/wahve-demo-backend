43e7120eb10d8f81ed3cf14511427a8db8fb9d6d
Julio Suarez <juliosuarezf93@gmail.com>
2025-09-23 23:51:16 -0400

    chore: get score function

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,25 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  private allTraits = ['Adaptability', 'Empathy', 'Harmony', 'Discipline', 'Focus'];
+
+  getScore(jobTraitRanking: string[], applicantTraitRanking: string[]) {
+    let score = 0;
+    const maxScore = this.allTraits.length * this.allTraits.length;
+
+    this.allTraits.forEach(trait => {
+      const jobI = jobTraitRanking.indexOf(trait);
+      const applicantI = applicantTraitRanking.indexOf(trait);
+
+      if (jobI < 0 || applicantI < 0) {
+        return;
+      }
+
+      const distance = Math.abs(jobI - applicantI);
+      score = score + (this.allTraits.length - distance);
+    });
+
+    return score / maxScore;
   }
 }
